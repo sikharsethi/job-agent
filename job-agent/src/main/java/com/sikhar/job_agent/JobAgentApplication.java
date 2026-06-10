7b9aea71a0b323215f78884d259baea2a97a9a64
Sikhar Sethi <sikharsethi5@gmail.com>
2026-06-10 18:05:59 +0530

    Initial project setup with Spring Boot and PostgreSQL

---
+++ job-agent/src/main/java/com/sikhar/job_agent/JobAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sikhar.job_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAgentApplication.class, args);
+	}
+
+}
